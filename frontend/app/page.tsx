58b2d8fa52edb59b5b8af08e72956d8d79a7a773
김규민 <abc2232002@gmail.com>
2025-08-24 10:37:18 +0900

    Next-Auth 회원가입, SignIn, SignUp 화면 구현 - Part2

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <div>인프런 클론</div>;
+import { auth } from "@/auth";
+import { signOut } from "@/auth";
+import Link from "next/link";
+
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <div>
+      <p>현재 로그인한 유저 보여주기</p>
+      <p>{session?.user?.email}</p>
+      {session?.user ? (
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit">로그아웃</button>
+        </form>
+      ) : (
+        <Link href="/signin">로그인</Link>
+      )}
+    </div>
+  );
 }
